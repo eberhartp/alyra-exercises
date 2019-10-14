4315d45d47d57655e01122cf46a7355727100df2
eberhartp <pacome.eberhart@gmail.com>
2019-10-14 14:43:20 +0200

    Ajout des différentes méthodes d'affichage

--- semaine01/affichage.js
+++ semaine01/affichage.js
@@ -1,2 +1,5 @@
 console.log("Hello, World");
-console.log(Math.floor(Math.random()*100+1));
+let randomNumber = Math.floor(Math.random()*100+1);
+console.log("The random number is", randomNumber);
+console.log(`The random number is ${randomNumber}`);
+console.log({randomNumber});
