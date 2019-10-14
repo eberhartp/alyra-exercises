2a3d9bd8d87076ae308e7ec49841ad290a1c2bae
eberhartp <pacome.eberhart@gmail.com>
2019-10-14 15:58:19 +0200

    Boucle palindrome coupée en deux et arrondie à l'inferieur, Ajout de test pour palindrome version boucle

--- semaine01/palindrome.js
+++ semaine01/palindrome.js
@@ -1,5 +1,5 @@
 function palindromeLoop(word){
-    for (let i = 0; i < word.length / 2; i++) {
+    for (let i = 0; i < Math.floor(word.length / 2); i++) {
         const element = word[i];
         if (element !== word[word.length - i - 1])
             return false;
@@ -15,10 +15,19 @@ function palindrome(word) {
     return false;
 }
 
+function isPalindromeLoop(string) {
+    console.log(string, palindromeLoop(string.split(" ").join("")) ? "is" : "is not", "a palindrome");
+}
 function isPalindrome(string) {
     console.log(string, palindrome(string.split(" ").join("")) ? "is" : "is not", "a palindrome");
 }
 
+isPalindromeLoop("BOB");
+isPalindromeLoop("BOKB");
+isPalindromeLoop("ANNA");
+isPalindromeLoop("ESOPE RESTE ICI ET SE REPOSE");
+
 isPalindrome("BOB");
+isPalindrome("BOKB");
 isPalindrome("ANNA");
 isPalindrome("ESOPE RESTE ICI ET SE REPOSE");
